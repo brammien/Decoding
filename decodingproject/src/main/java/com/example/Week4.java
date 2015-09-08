02d2df537290ec3185e40aef1d5ff6bef577a4b4
Brian A Rammien <brammien@e104-m-11.mchenry.edu>
2015-09-08 13:20:51 -0500

    Initial commit

---
+++ decodingproject/src/main/java/com/example/Week4.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Week4 {
+
+    public static void main(String[] args) {
+
+
+
+        System.out.println(" Go to the red door and ");
+        System.out.println(" knock \u0046\u004F\u0055\u0052 \u0054\u0049\u004D\u0045\u0053");
+    }
+}
+
+
+
